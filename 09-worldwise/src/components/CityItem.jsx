44d2083fc9e6c32fc65d518ff7a7d225c6d9c8d2
bigyanrai <9813199661now@gmail.com>
2025-01-02 20:00:20 +0545

    feat: fix

--- 09-worldwise/src/components/CityItem.jsx
+++ 09-worldwise/src/components/CityItem.jsx
@@ -30,7 +30,7 @@ function CityItem({ city }) {
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
-        <button className={styles.delelteBtn} onClick={handleClick}>
+        <button className={styles.deleteBtn} onClick={handleClick}>
           &times;
         </button>
       </Link>
